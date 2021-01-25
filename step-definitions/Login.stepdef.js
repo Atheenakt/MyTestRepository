9495ab6f1d8fd49d6eede1f620aa0830e10afb4c
Atheenakt <77928157+Atheenakt@users.noreply.github.com>
2021-01-25 10:54:07 +0530

    Update Login.stepdef.js

--- step-definitions/Login.stepdef.js
+++ step-definitions/Login.stepdef.js
@@ -14,6 +14,26 @@ When(/^User enters invalid email address and password$/, function() {
 });
 Then(/^User should see a message saying "Authentication failed"$/, function() {
     var loginmessage=$("//div[@Class='alert alert-danger']/ol/li").getText();
-    console.log("Message from find element is "+loginmessage);
+    console.log(loginmessage);
+    
+});
+
+Given(/^user follows "Sign In" from home page$/, function() {
+    browser.url("http://automationpractice.com/index.php");
+    $("//a[@class='login']").click();   
+});
+
+When(/^User enters valid email address and password$/, function() {   
+    console.log("inside function");
+    browser.$("//input[@id='email']").setValue("TestUser124@gmail.com")
+    browser.$("//input[@id='passwd']").setValue("Test@1234");
+    browser.$("//button[@id='SubmitLogin']").click(); 
+   
+});
+Then(/^User should be able to see Myaccount page$/, function() {
+    $("//h1[@class='page-heading']").isDisplayed()
+    {
+        console.log("Logged in to the account successfully");
+    }
     
 });
